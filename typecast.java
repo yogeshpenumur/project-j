fe20cde312344811910add6968dc1f9bcdc9ff3c
yogesh <yogeshpenumur2002@gmail.com>
2024-07-13 20:56:29 +0530

    feat: file upload

---
+++ typecast.java
@@ -0,0 +1,12 @@
+public class typecast {
+    public static void main(String...args)
+      {
+        int a;
+        int b;
+        a=99;
+        b=++a + --a - a++ + a++ - --a;
+        System.out.println(b);123456789ghvuhgvuvugbvygu
+
+      }
+    
+}
